cfbf6e943421ddc5307eaa55aa840f00b858152a
Ruslan Shaydulin <rshaydu@g.clemson.edu>
2018-09-03 13:12:11 -0400

    isolated the function that runs the ansatz

---
+++ ibmqxbackend/ansatz.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from qiskit import register, available_backends
+from qiskit import get_backend, compile
+from ibmqxbackend.aqua.ryrz import VarFormRYRZ
+
+class IBMQXVarForm(object):
+    """
+    Connection to IBM Quantum Experience that runs ansatz on IBM backend (be it simulator or physical device)
+
+    By default uses Qconfig.py file in the root folder of ibmqxbackend module
+    """
+
+    def __init__(self, var_form='RYRZ', num_qubits=10, depth=3):
+        try:
+            import sys
+            sys.path.append("../") # go to parent dir
+            import Qconfig
+        except Exception as e:
+            print(e)
+        
+        APItoken=getattr(Qconfig, 'APItoken', None)
+        url = Qconfig.config.get('url', None)
+        hub = Qconfig.config.get('hub', None)
+        group = Qconfig.config.get('group', None)
+        project = Qconfig.config.get('project', None)
+        try:
+            register(APItoken, url, hub, group, project)
+        except Exception as e:
+            print(e)
+
+        if var_form == 'RYRZ':
+            self.var_form = VarFormRYRZ()
+            self.var_form.init_args(num_qubits, depth, entanglement='linear')
+            self.num_parameters = self.var_form._num_parameters
+
+    def run(self, parameters, backend_name="local_qasm_simulator"):
+        res = {'backend_name':backend_name, 'parameters':parameters}
+        qc = self.var_form.construct_circuit(parameters)
+
+        # kinda hacky
+        qc.measure(qc.get_qregs()['q'], qc.get_cregs()['c'])
+
+        res['uncompiled_qasm'] = qc.qasm()
+        backend = get_backend(backend_name)
+        qobj = compile(qc, backend=backend)
+        
+        #res['compiled_qasm'] = qobj.experiments[0].header.compiled_circuit_qasm
+        res['compiled_qasm'] = None 
+        res['job'] = backend.run(qobj)
+        res['result'] = res['job'].result()
+        
+        return res 
--- ibmqxbackend/benchmark_ansatz.py
+++ ibmqxbackend/benchmark_ansatz.py
@@ -6,70 +6,29 @@
 #
 
 # Import the Qiskit SDK
-from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
-from qiskit import available_backends, execute, get_backend, compile
+from qiskit import available_backends
 import time
 import numpy as np
 from difflib import ndiff
 import argparse
 #from qiskit.backends.jobstatus import JobStatus, JOB_FINAL_STATES
+from ibmqxbackend.ansatz import IBMQXVarForm
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-q", type=int, default=10, help="number of qubits")
 args = parser.parse_args()
 
-try:
-    import sys
-    sys.path.append("../") # go to parent dir
-    import Qconfig
-except Exception as e:
-    print(e)
-    
-from qiskit import register, available_backends
+var_form = IBMQXVarForm(num_qubits=args.q, depth=3)
 
-#set api
-APItoken=getattr(Qconfig, 'APItoken', None)
-url = Qconfig.config.get('url', None)
-hub = Qconfig.config.get('hub', None)
-group = Qconfig.config.get('group', None)
-project = Qconfig.config.get('project', None)
-try:
-    register(APItoken, url, hub, group, project)
-except Exception as e:
-    print(e)
-
-#-------------------------------------------------- 
-# TODO: insert variational form ansatz
-# ./qiskit_aqua/utils/variational_forms/ryrz.py or smth
-#-------------------------------------------------- 
-
-from ibmqxbackend.aqua.ryrz import VarFormRYRZ
-
-var_form = VarFormRYRZ()
-num_qubits = args.q
-depth = 3
-var_form.init_args(num_qubits, depth, entanglement='linear')
-parameters = np.random.uniform(-np.pi, np.pi, var_form._num_parameters)
-
-qc = var_form.construct_circuit(parameters)
-
-# relies on hacked VarFormRYRZ
-qc.measure(qc.get_qregs()['q'], qc.get_cregs()['c'])
-
-uncompiled_qasm = qc.qasm()
+parameters = np.random.uniform(-np.pi, np.pi, var_form.num_parameters)
 
 # See a list of available local simulators
-#print("Backends: ", available_backends(compact=False))
+print("Backends: ", available_backends(compact=False))
 
 #backend_name = "ibmq_5_tenerife"
 #backend_name = "ibmq_16_rueschlikon"
 backend_name = "local_qasm_simulator_cpp"
-
-# Compile and run the Quantum circuit on a simulator backend
-my_backend = get_backend(backend_name)
-qobj = compile(qc, backend=my_backend)
-
-#compiled_qasm = qobj.experiments[0].header.compiled_circuit_qasm
+#backend_name = "local_qasm_simulator_py"
 
 if False:
     # set to True to see what changed in compilation
@@ -83,7 +42,6 @@ print("running on {}...".format(backend_name))
 import timeit
 
 start_time = timeit.default_timer()
-job = my_backend.run(qobj)
 #lapse = 0
 #interval = 1
 #while True:
@@ -96,10 +54,11 @@ job = my_backend.run(qobj)
 #
 #print(job.status)
 
-result = job.result()
+res = var_form.run(parameters, backend_name=backend_name)
+
 elapsed = timeit.default_timer() - start_time
 print("For {} qubits finished in {}".format(args.q,elapsed))
-print(result)
+print(res['result'])
 # Show the results
 #counts = result.get_counts()
 #print(counts)
