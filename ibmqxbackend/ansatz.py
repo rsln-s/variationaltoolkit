79d2a79059f00f65d1d13475aca6ff931dbef3e0
Ruslan Shaydulin <rshaydu@g.clemson.edu>
2018-11-01 16:18:19 -0400

    added classical qaoa

--- ibmqxbackend/ansatz.py
+++ ibmqxbackend/ansatz.py
@@ -3,6 +3,8 @@
 from qiskit import register, IBMQ, Aer
 from qiskit import get_backend, compile, QISKitError
 from ibmqxbackend.aqua.ryrz import VarFormRYRZ
+from ibmqxbackend.aqua.qaoa import QAOAVarForm
+from ibmqxbackend.aqua.modularity_ising import get_modularity_qubitops
 from time import sleep
 import os
 import logging
@@ -14,7 +16,7 @@ class IBMQXVarForm(object):
     By default uses Qconfig.py file in the root folder of ibmqxbackend module
     """
 
-    def __init__(self, num_qubits=10, depth=3, var_form='RYRZ', APItoken=None):
+    def __init__(self, num_qubits=10, depth=3, var_form='RYRZ', APItoken=None, B=None):
         if len(IBMQ.stored_accounts()) <= 1:
             # if didn't register yet
             if APItoken is None:
@@ -28,9 +30,14 @@ class IBMQXVarForm(object):
         if var_form == 'RYRZ':
             self.var_form = VarFormRYRZ()
             self.var_form.init_args(num_qubits, depth, entanglement='linear')
-            self.num_parameters = self.var_form._num_parameters
+        elif var_form == 'QAOA':
+            if B is None:
+                raise ValueError("If using var_form == QAOA, have to specify B")
+            qubitOp = get_modularity_qubitops(B)
+            self.var_form = QAOAVarForm(qubitOp, depth)
         else:
             raise ValueError("Incorrect var_form {}".format(var_form))
+        self.num_parameters = self.var_form._num_parameters
         logging.info("Initialized IBMQXVarForm {} with num_qubits={}, depth={}".format(var_form, num_qubits, depth))
 
     def run(self, parameters, backend_name="local_qasm_simulator", return_all=False, samples=1000, seed=42, nattempts=25):
---
+++ ibmqxbackend/aqua/modularity_ising.py
@@ -0,0 +1,21 @@
+"""
+
+Generates cost Hamiltonian for the modularity maximization problem
+
+"""
+import numpy as np
+from qiskit.tools.qi.pauli import Pauli
+from qiskit_aqua import Operator
+
+def get_modularity_qubitops(B):
+    num_nodes = B.shape[0]
+    pauli_list = []
+    for i in range(num_nodes):
+        for j in range(i):
+            if (B[i,j] != 0):
+                wp = np.zeros(num_nodes)
+                vp = np.zeros(num_nodes)
+                vp[i] = 1
+                vp[j] = 1
+                pauli_list.append([2.0 * B[i, j], Pauli(vp, wp)])
+    return Operator(paulis=pauli_list)
