c2d25770e29f7a4ce3d1545d5ee10e6d9399cb2a
Ruslan Shaydulin <rshaydu@g.clemson.edu>
2019-06-07 14:06:03 -0400

    added updated benchmarking script

--- ibmqxbackend/benchmark_ansatz.py
+++ ibmqxbackend/benchmark_ansatz.py
@@ -9,17 +9,50 @@ import argparse
 import timeit
 import logging
 import numpy as np
-from qcommunity.optimization.obj import get_obj_val
+import networkx as nx
+import csv
+from pathlib import Path
+from qiskit import Aer, execute
+from ibmqxbackend.aqua.qaoa import QAOAVarForm
+from ibmqxbackend.aqua.modularity_ising import get_modularity_qubitops
+from qcommunity.utils.import_graph import generate_graph
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-q", type=int, default=5, help="number of qubits / 2 (total number of qubits is 2*q)")
+parser.add_argument("-threads", type=int, default=1, help="number of threads qiskit aer is using")
+parser.add_argument(
+    "--save",
+    help="saves summarized results as a csv, with name as parameter. If csv exists, it appends to the end",
+    type=str)
 args = parser.parse_args()
 
 logging.basicConfig(level=logging.INFO)
 
 p = 2
-obj_val, num_parameters = get_obj_val("get_connected_caveman_graph", 2, args.q, seed=1, obj_params='ndarray', backend='IBMQX', backend_params={'backend_device': 'qasm_simulator', 'depth': p, 'var_form':'QAOA'}, sign=-1)
-y = np.random.uniform(-np.pi, np.pi, num_parameters)
+backend = Aer.get_backend("qasm_simulator")
+
+G, _ = generate_graph("get_connected_caveman_graph", 2, args.q)
+B = nx.modularity_matrix(G, weight='weight').A
+qubitOp = get_modularity_qubitops(B)
+var_form = QAOAVarForm(qubitOp, p)
+parameters = np.random.uniform(-np.pi, np.pi, 2*p)
+qc = var_form.construct_circuit(parameters)
+qc.measure(qc.qregs[0], qc.cregs[0])
 start_time = timeit.default_timer()
-print(obj_val(y))
-print("Finished in: {:.2f} sec".format(timeit.default_timer() - start_time))
+qobj = execute(qc, backend=backend, backend_options = {"max_parallel_threads" : args.threads})
+res = qobj.result()
+runtime = timeit.default_timer() - start_time
+print("Finished in: {:.2f} sec on {} threads".format(runtime, args.threads))
+header = ['nqubits', 'nthreads', 'runtime (sec)']
+results = [args.q, args.threads, runtime]
+if args.save:
+    if Path(args.save).exists():
+        write_header = False
+    else:
+        write_header = True
+
+    with open(args.save, 'a') as csvfile:
+        out = csv.writer(csvfile)
+        if write_header:
+            out.writerow(header)
+        out.writerow(results)
