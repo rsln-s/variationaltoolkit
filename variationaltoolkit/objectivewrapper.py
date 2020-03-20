a4e18272a567b6f08eb486b472cd4601f505ac7d
Joey LIU <xiaoyu3@g.clemson.edu>
2020-03-19 23:49:34 -0400

    fix varform if using mpsbackend

--- variationaltoolkit/objectivewrapper.py
+++ variationaltoolkit/objectivewrapper.py
@@ -37,7 +37,7 @@ class ObjectiveWrapper:
         self.execute_parameters = execute_parameters
         self.objective_parameters = objective_parameters
         self.backend_description = backend_description
-        if 'package' in self.varform_description and self.varform_description['package'] == 'mpsbackend':
+        if self.backend_description['package'] == 'mpsbackend':
             import mpsbackend.variational_forms as mps_variational_forms
             varform_parameters = {k : v for k,v in varform_description.items() if k != 'name' and k != 'package'}
             self.var_form = getattr(mps_variational_forms, varform_description['name'])(**varform_parameters)
